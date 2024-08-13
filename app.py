6481b6d040946bb770cf1ece4107ca7e14605cf0
Prayas <prayaspatel199@gmail.com>
2024-08-13 15:16:22 +0530

    hehehahha

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World! ahhahahaahahahahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+    return "iufghfvhhjgfhgkjmvnbvhbnbn vbvbvmn nvhbn bnvhvn  ygguy yggtyr yfgfmfyn"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
